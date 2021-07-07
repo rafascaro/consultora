8f7977c528e33517c8a8588b855f33bc65232b90
Rafael Caro <rafael.caro@iprospect.com>
2021-07-07 10:15:48 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <body class="bg-red-500">
+    <div class="flex h-screen">
+      <div class="m-auto p-8 max-w-md md:max-w-2xl bg-transparent rounded">
+        <h1 class="uppercase tracking-wide">Consultora</h1>
+        <h2 class="uppercase tracking-wide">Loica</h2>
+      </div>
+    </div>
+  </body>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
